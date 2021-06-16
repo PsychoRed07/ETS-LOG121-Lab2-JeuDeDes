60ea0abd7b6febf43db34b1e4855b310bc60e264
Megane Darnaud <meganedarnaud@gmail.com>
2021-06-16 11:59:04 -0400

    Added package game elements with dice and player classes

---
+++ Project/GameElements/Dice.java
@@ -0,0 +1,19 @@
+/******************************************************
+ Cours:   LOG121
+ Session: E2021
+ Groupe:  01
+ Projet: TP #2
+ Étudiant(e)s: Ariane Ouellette, Hicham Belkaid, Megane Darnaud, Karim Ibrahim
+ Professeur : Vincent Lacasse
+ Nom du fichier: Dice.java
+ Date créé: 2021-06-16
+ Date dern. modif. 2021-07-
+ *******************************************************/
+
+public class Dice implements Comparable<Dice>{
+
+    @Override
+    public int compareTo(Dice d) {
+        return 0;
+    }
+}
---
+++ Project/GameElements/Player.java
@@ -0,0 +1,19 @@
+/******************************************************
+ Cours:   LOG121
+ Session: E2021
+ Groupe:  01
+ Projet: TP #2
+ Étudiant(e)s: Ariane Ouellette, Hicham Belkaid, Megane Darnaud, Karim Ibrahim
+ Professeur : Vincent Lacasse
+ Nom du fichier: Player.java
+ Date créé: 2021-06-16
+ Date dern. modif. 2021-07-
+ *******************************************************/
+
+public class Player implements Comparable<Player> {
+
+    @Override
+    public int compareTo(Player o) {
+        return 0;
+    }
+}
