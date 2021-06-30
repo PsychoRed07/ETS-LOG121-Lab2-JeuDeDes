82d94127c83d61321aae0e36da152b945fe52fb6
Megane Darnaud <meganedarnaud@gmail.com>
2021-06-30 18:14:26 -0400

    Player junit tests start

--- src/test/main/java/com/project/generic/PlayerTest.java
+++ src/test/main/java/com/project/generic/PlayerTest.java
@@ -1,64 +1,65 @@
 package main.java.com.project.generic;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerTest {
+    private Player p1;
+    private Player p2;
+
+    @BeforeEach
+    public void doBefore(){
+        p1 = new Player(0);
+        p2 = new Player(1);
+    }
 
     @Test
     void getID() {
-        Player p = new Player(0);
-        assertEquals(0, p.getID());
+        assertEquals(0, p1.getID());
     }
 
     @Test
     void setID() {
-        Player p = new Player(0);
-        p.setID(1);
-        assertEquals(1, p.getID());
+        p1.setID(2);
+        assertEquals(2, p1.getID());
     }
 
     @Test
     void getTurn() {
-        Player p = new Player(0);
-        p.setTurn(1);
-        assertEquals(1, p.getTurn());
+        p1.setTurn(1);
+        assertEquals(1, p1.getTurn());
     }
 
     @Test
     void setTurn() {
-        Player p = new Player(0);
-        p.setTurn(1);
-        assertEquals(1, p.getTurn());
+        p1.setTurn(1);
+        assertEquals(1, p1.getTurn());
     }
 
     @Test
     void getDiceAmount() {
-        Player p = new Player(0);
-        p.setDiceAmount(1);
-        assertEquals(1, p.getDiceAmount());
+        p1.setDiceAmount(3);
+        assertEquals(3, p1.getDiceAmount());
     }
 
     @Test
     void setDiceAmount() {
-        Player p = new Player(0);
-        p.setDiceAmount(1);
-        assertEquals(1, p.getDiceAmount());
+        p1.setDiceAmount(3);
+        assertEquals(3, p1.getDiceAmount());
     }
 
     @Test
     void getPlayerScore() {
-        Player p = new Player(0);
-        p.setPlayerScore(10);
-        assertEquals(10, p.getPlayerScore());
+        p1.setPlayerScore(10);
+        assertEquals(10, p1.getPlayerScore());
     }
 
     @Test
     void setPlayerScore() {
-        Player p = new Player(0);
-        p.setPlayerScore(10);
-        assertEquals(10, p.getPlayerScore());
+        p1.setPlayerScore(10);
+        assertEquals(10, p1.getPlayerScore());
     }
 
     @Test
