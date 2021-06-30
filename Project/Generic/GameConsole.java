5577f5ceadd74db7d64057dc251ce513beedbb50
Karim Ibrahim <chillman359@gmail.comm>
2021-06-30 15:39:04 -0400

    Changed GameDemo to GameConsole

--- Project/Generic/GameConsole.java
+++ Project/Generic/GameConsole.java
@@ -16,7 +16,7 @@ import Bunco.BuncoTemplate;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class GameDemo {
+public class GameConsole {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(new InputStreamReader(System.in));
