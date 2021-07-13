553bb5552da9374dda959dab1ceebb07ac6d23a3
Karim Ibrahim <chillman359@gmail.com>
2021-07-13 16:34:13 -0400

    Did some tests for Iterator. Deleted Bunco.java.

--- src/test/main/java/com/project/generic/IteratorTest.java
+++ src/test/main/java/com/project/generic/IteratorTest.java
@@ -1,20 +1,45 @@
 package main.java.com.project.generic;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class IteratorTest {
 
+    Creator creator;
+    Iterator iterator;
+
+    @BeforeEach
+    void doBefore() {
+        creator = new Creator();
+        Collection collection = creator.createPlayers(5);
+        iterator = collection.createIterator();
+    }
+
     @Test
     void hasNext() {
+        int count = 0;
+        while (iterator.hasNext()){
+            Object o = iterator.next();
+            count++;
+        }
+        assertEquals(count, 5);
     }
 
     @Test
     void next() {
+        int count = 0;
+        while (iterator.hasNext()){
+            Player o = (Player) iterator.next();
+            count++;
+            assertEquals(o.getID(), count);
+        }
+
     }
 
     @Test
     void reset() {
+
     }
 }
